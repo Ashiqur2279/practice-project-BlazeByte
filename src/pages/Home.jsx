f14fa5a48c6d75295df3340bbab017744a525abb
Ashiqur Rahman <ashiqurrahman2279@gmail.com>
2025-04-06 23:03:23 +0600

    The project sculpture is ready.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      this is home page
+    </div>
+  );
+};
+
+export default Home;
