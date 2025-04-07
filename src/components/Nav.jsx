f881b3c82fae6d19aa3e789be205564c572d266a
Ashiqur Rahman <ashiqurrahman2279@gmail.com>
2025-04-07 07:29:25 +0600

    feat: add theme toggle with persistent state using localStorage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,67 +1,80 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Nav = () => {
-  return (
-    <div>
-      <div className="navbar bg-base-100 top-0 sticky shadow-xl z-10">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-2xl font-bold gap-0">
-            <span className="text-secondary">Byte</span>
-            <span className="text-primary">Blaze</span>
-          </a>
+    const [theme, setTheme] = useState("light");
+    const handleThemeToggle = (event) => {
+        if (event.target.checked) {
+            setTheme("synthwave");
+        } else setTheme("light");
+    };
+    useEffect(() => {
+        localStorage.setItem('localTheme', theme)
+        const currentLocalTheme = localStorage.getItem('localTheme')
+        document.querySelector("html").setAttribute("data-theme", currentLocalTheme);
+    }, [theme])
+    return (
+        <div>
+            <div className="navbar bg-base-100 top-0 sticky shadow-xl z-10">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-2xl font-bold normal-case gap-0">
+                        <span className="text-secondary">Byte</span>
+                        <span className="text-primary">Blaze</span>
+                    </a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li className="font-bold">
+                            <a>Home</a>
+                        </li>
+                        <li className="font-bold">
+                            <a>Blog</a>
+                        </li>
+                        <li className="font-bold">
+                            <a>Bookmarks</a>
+                        </li>
+                        <li>
+                            <label className="flex cursor-pointer gap-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <circle cx="12" cy="12" r="5"/>
+                                    <path
+                                        d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/>
+                                </svg>
+                                <input
+                                    onChange={handleThemeToggle}
+                                    type="checkbox"
+                                    value="synthwave"
+                                    className="toggle theme-controller"
+                                />
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                                </svg>
+                            </label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li className="font-bold">
-              <a>Home</a>
-            </li>
-            <li className="font-bold">
-              <a>Blog</a>
-            </li>
-            <li className="font-bold">
-              <a>Bookmarks</a>
-            </li>
-            <li>
-              <label className="flex cursor-pointer gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <circle cx="12" cy="12" r="5" />
-                  <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-                </svg>
-                <input
-                  type="checkbox"
-                  value="synthwave"
-                  className="toggle theme-controller"
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                </svg>
-              </label>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Nav;
--- src/components/WelcomeText.jsx
+++ src/components/WelcomeText.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 const WelcomeText = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h1>Welcome to ByteBlaze</h1>
+    <div className=" text-center p-4">
+      <h1 className="text-3xl font-bold">
+        Welcome to <span className="text-primary">ByteBlaze</span>
+      </h1>
       <p>
         ByteBlaze is the bridge between the complex world of technology and the
         curious minds eager to understand it
       </p>
+      <div className="space-x-2">
+        <button className="btn btn-primary">Blogs</button>
+        <button className="btn btn-secondary">Bookmarks</button>
+      </div>
     </div>
   );
 };
